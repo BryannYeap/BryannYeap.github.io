5c3cf9b9d2b0a8fc0efd0e7acaedd9a5da9c733d
BryannYeap <e0543723@u.nus.edu>
2022-11-01 02:22:40 +0800

    Add GA4 measurement ID

--- src/index.js
+++ src/index.js
@@ -6,14 +6,22 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 import ReactGA from "react-ga";
+import GA4React from "ga-4-react";
 
 const TRACKING_ID = "UA-247681701-1";
 ReactGA.initialize(TRACKING_ID);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-);
+
+const ga4react = new GA4React("G-BC295HJ8Z3");
+
+(async (_) => {
+  await ga4react.initialize().finally(() => {
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(
+      <React.StrictMode>
+        <Router>
+          <App />
+        </Router>
+      </React.StrictMode>
+    );
+  });
+})();
